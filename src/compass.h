0c955d2d1ad7dff4ce77df20daa533db9d4424f2
WJQ <wjquigs@gmail.com>
2024-08-21 17:15:41 -0700

    switched load cell driver

--- src/compass.h
+++
@@ -1,9 +0,0 @@
-// compass control structure
-typedef struct control_s {
-  bool compassOnToggle = true;
-  int orientation = 0;
-  int variation = 0;
-  int frequency = 100;
-  char hostname[64] = "";
-} control_s;
-extern control_s compassParams;
--- src/webserver.cpp
+++ src/webserver.cpp
@@ -13,8 +13,6 @@
 #include <SPIFFS.h>
 #include <math.h>
 #include "Async_ConfigOnDoubleReset_Multi.h"
-#include "compass.h"
-#include <esp_now.h>
 
 double rad=0.01745;
 
@@ -32,15 +30,6 @@ extern unsigned long lastTime;
 extern int WebTimerDelay;
 extern int minReadRate;
 
-extern bool gameRot, absRot;
-
-float calculateHeading(float r, float i, float j, float k, int correction);
-float calculateHeading2(float r, float i, float j, float k);
-void setReports();
-extern float heading, heading2, accuracy;
-
-extern esp_now_peer_info_t peerInfo;
-
 // Get Sensor Readings and return JSON object
 String getSensorReadings() {
   // readings set in getMastHeading()
@@ -54,10 +43,10 @@ String getSensorReadings() {
 String processor(const String& var){
   //Serial.println(var);
   if(var == "VARIATION"){
-    return String(compassParams.variation);
+    return String(0);
   }
   else if(var == "ORIENTATION"){
-    return String(compassParams.orientation);
+    return String(1);
   }
   else if(var == "TIMERDELAY"){
     return String(WebTimerDelay);
@@ -83,8 +72,8 @@ void startWebServer() {
   });
 
   server.on("/host", HTTP_GET, [](AsyncWebServerRequest *request) {
-    String buf = "hostname: " + host + ", variation: " + String(compassParams.variation) + ", orientation: " + String(compassParams.orientation) + ", timerdelay: " + String(WebTimerDelay) + "\n";
-    buf += "ESP local MAC addr: " + String(WiFi.macAddress() + "\n");
+    String buf = "hostname: " + host;
+    buf += " ESP local MAC addr: " + String(WiFi.macAddress() + "\n");
     logToAll(buf);
     request->send_P(200, "text/plain", buf.c_str());
     buf = String();
@@ -93,56 +82,13 @@ void startWebServer() {
   server.on("/config", HTTP_GET, [](AsyncWebServerRequest *request) {
     logToAll("config\n");
     String response = "none";
-    if (request->hasParam("orientation")) {
-      compassParams.orientation = atoi(request->getParam("orientation")->value().c_str());
-      response = "change orientation to " + String(compassParams.orientation) + "\n";
-      logToAll(response);
-      preferences.putInt("orientation", compassParams.orientation);
-    } else if (request->hasParam("variation")) {
-      compassParams.variation = atoi(request->getParam("variation")->value().c_str());
-      response = "change variation to " + String(compassParams.variation) + "\n";
-      logToAll(response);
-      preferences.putInt("variation",compassParams.variation);
-    } else if (request->hasParam("timerdelay")) {
-      WebTimerDelay = atoi(request->getParam("timerdelay")->value().c_str());
-      if (WebTimerDelay < minReadRate) WebTimerDelay = minReadRate;
-      response = "change WebTimerDelay to " + String(WebTimerDelay) + "\n";
-      logToAll(response);
-      preferences.putInt("timerdelay",WebTimerDelay);
-    } else if (request->hasParam("gameRot")) {
-      gameRot = (request->getParam("gameRot")->value().equals("true")) ? true : false;
-      response = "change gameRot to " + String(gameRot) + "\n";
-      logToAll(response);
-      preferences.putBool("gameRot",gameRot);
-      setReports();
-    } else if (request->hasParam("absRot")) {
-      Serial.printf("absrot %s\n", request->getParam("absRot")->value().c_str());
-      absRot = (request->getParam("absRot")->value().equals("true")) ? true : false;
-      response = "change absRot to " + String(absRot) + "\n";
-      logToAll(response);
-      preferences.putBool("absRot",absRot);
-      setReports();
-    }  else if (request->hasParam("hostname")) {
+    if (request->hasParam("hostname")) {
       Serial.printf("hostname %s\n", request->getParam("hostname")->value().c_str());
       host = request->getParam("hostname")->value();
       response = "change hostname to " + host + "\n";
       logToAll(response);
       preferences.putString("hostname",host);
-    }  else if (request->hasParam("frequency")) {
-      compassParams.frequency = atoi(request->getParam("frequency")->value().c_str());
-      Serial.printf("frequency %d\n", compassParams.frequency);
-      response = "change frequency to " + String(compassParams.frequency) + "\n";
-      logToAll(response);
-      preferences.putInt("frequency",compassParams.frequency); 
-    }  else if (request->hasParam("toggle")) {
-      Serial.printf("toggle %s\n", request->getParam("toggle")->value().c_str());
-      compassParams.compassOnToggle = (request->getParam("toggle")->value().equals("true")) ? true : false;
-      response = "change toggle to " + String(compassParams.compassOnToggle) + "\n";
-      logToAll(response);
-      preferences.putBool("toggle",compassParams.compassOnToggle);
-    } 
-    //request->send(SPIFFS, "/index.html", "text/html");
-    //request->redirect("/index.html");
+    }
     request->send_P(200, "text/plain", response.c_str());
     response = String();
   }); 
